4f97987784ee29acb564c010e6b38e72b51fc314
thihazawsatellie <thihazawsatellite@gmail.com>
2026-02-23 22:45:25 +0630

    Add M3U file generation

--- index.js
+++ index.js
@@ -1,35 +1,39 @@
-const { onSchedule } = require("firebase-functions/v2/scheduler");
-const admin = require("firebase-admin");
-const axios = require("axios");
+const fs = require('fs');
+const axios = require('axios');
+const admin = require('firebase-admin');
 
-admin.initializeApp();
+if (!admin.apps.length) {
+    admin.initializeApp();
+}
+const db = admin.firestore();
 
-exports.syncOxaxChannels = onSchedule("every 1 hours", async (event) => {
-  const db = admin.firestore();
-  
-  try {
-    // oxax.tv ရဲ့ data source (JSON) ကို ဒီမှာယူပါမယ်
-    const response = await axios.get("https://oxax.tv/api/links"); 
-    const channels = response.data; 
+async function updateIPTV() {
+    try {
+        const response = await axios.get('https://oxax.tv/api/links');
+        const channels = response.data;
+        
+        let m3uContent = '#EXTM3U\n';
+        const batch = db.batch();
 
-    const batch = db.batch();
+        channels.forEach((ch) => {
+            m3uContent += "#EXTINF:-1 tvg-name=\"" + ch.name + "\"," + ch.name + "\n" + ch.stream_url + "\n";
+            
+            const docId = ch.id || ch.name.replace(/\s+/g, '-').toLowerCase();
+            const docRef = db.collection('channels').doc(docId);
+            batch.set(docRef, {
+                name: ch.name,
+                stream_url: ch.stream_url,
+                updated_at: admin.firestore.FieldValue.serverTimestamp()
+            }, { merge: true });
+        });
 
-    channels.forEach((ch) => {
-      // Channel တစ်ခုချင်းစီကို Database ထဲမှာ Update လုပ်မယ်
-      // ID မရှိရင် နာမည်ကို ID အဖြစ် ပြောင်းသုံးပါမယ်
-      const docId = ch.id || ch.name.replace(/\s+/g, '-').toLowerCase();
-      const docRef = db.collection("channels").doc(docId);
-      
-      batch.set(docRef, {
-        name: ch.name,
-        stream_url: ch.url,
-        updated_at: admin.firestore.FieldValue.serverTimestamp()
-      }, { merge: true });
-    });
+        fs.writeFileSync('playlist.m3u', m3uContent);
+        await batch.commit();
+        
+        console.log("Successfully updated Firebase and playlist.m3u!");
+    } catch (error) {
+        console.error("Update Error:", error);
+    }
+}
 
-    await batch.commit();
-    console.log("Channels synced successfully to Firebase!");
-  } catch (error) {
-    console.error("Sync Error:", error);
-  }
-});
+updateIPTV();
