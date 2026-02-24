c42a2c4158005b604fac2e81ef015f30372c75ef
Daniel <88990819+thihazawsatellite@users.noreply.github.com>
2026-02-25 02:03:55 +0630

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const fs = require('fs');
 
-// မိတ်ဆွေရဲ့ Cloudflare Worker Base Link
 const workerUrl = "https://oxax-proxy.thihazawsatellite.workers.dev/?id=";
 
-// အလုပ်လုပ်မယ့် Channel ID တွေနဲ့ နာမည်တွေ
 const channels = [
-    { name: "Ox-Ax HD", id: "oh-ah" },
+    { name: "Oh Ah", id: "oh-ah" },
     { name: "Brazzers TV", id: "brazzers" },
     { name: "Private TV", id: "private" },
     { name: "Kino XXX", id: "kino" }
@@ -13,15 +11,10 @@ const channels = [
 
 function generateM3U() {
     let m3uContent = "#EXTM3U\n";
-
-    channels.forEach(channel => {
-        m3uContent += `#EXTINF:-1,${channel.name}\n`;
-        // ဒီနေရာမှာ Worker Link နဲ့ ID ကို ပေါင်းပေးလိုက်တာပါ
-        m3uContent += `${workerUrl}${channel.id}\n`;
+    channels.forEach(ch => {
+        m3uContent += `#EXTINF:-1,${ch.name}\n${workerUrl}${ch.id}\n`;
     });
-
     fs.writeFileSync('playlist.m3u', m3uContent);
-    console.log("M3U Playlist Updated!");
 }
 
 generateM3U();
